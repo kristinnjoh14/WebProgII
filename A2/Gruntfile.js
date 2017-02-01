ea772cc4bee544105d8ea44b00c81bf6d4a94de2
Kiddi <kiddinn95@gmail.com>
2017-02-01 19:15:13 +0000

    A2 pt 1

--- A2/Gruntfile.js
+++ A2/Gruntfile.js
@@ -1,12 +1,24 @@
 module.exports = function ( grunt ) {
  grunt.loadNpmTasks('grunt-contrib-jshint');
+ grunt.loadNpmTasks('grunt-contrib-uglify');
  var taskConfig = {
    jshint: {
-     src: ['**/*.js'],
+     src: ['*.js', "!Gruntfile.js"],
      gruntfile: ['Gruntfile.js'],
      options: {
+     	globals: {
+          jQuery: true
+        }
      }
+   },
+   uglify: {
+   	src: ['*.js'],
+   	gruntfile: ['Gruntfile.js'],
+   	options: {
+   		banner: '/*\n <%= pkg.name %> <%= grunt.template.today("yyyy-mm-dd") %> \n*/\n'
+   	}
    }
  };
  grunt.initConfig(taskConfig);
-}
+ grunt.registerTask('default', ['jshint']);
+};
---
+++ A2/index.js
@@ -0,0 +1,58 @@
+helloWorld();
+function helloWorld() {
+	console.log("Hello world");
+}
+helloWorld();
+var hello = helloWorld;
+hello();
+hello.call();
+var numbers = [ 0, 1, 2, 3, 4, 5, 6, 7, 8, 9 ];
+function printArray(array) {
+	for(i = 0; i < 10; i++) {
+		console.log(array[i]);
+	}
+}
+printArray(numbers);
+function printArray2(array) {
+	for(var num in array) {
+		console.log(num);
+	}
+}
+printArray2(numbers);
+function printArray3(array) {
+	array.forEach(function (currentValue) { console.log(currentValue); });
+}
+printArray3(numbers);
+var p = {name:"Kristinn Jóhannsson", age:21};
+function Person(name, age) {
+	this.name = name;
+	this.age = age;
+	return this;
+}
+var p2 = Person("Kiddi", 21);
+console.log(p.name, p.age);
+console.log(p2.name, p2.age);
+var f = function(){
+    return "Hello world"; 
+};
+
+//----------------------------------
+
+console.log(f);
+function DoStuff(x,y){
+    console.log(y); 
+}
+
+DoStuff(10);
+var arr = [1,2,3,4,5];
+console.log(arr.splice(1,2));
+var arr = [1,2,3,4,5];
+console.log(arr.slice(1,2));
+var arr = [1,2,3,4,5];
+for ( var i in arr ){
+    console.log( arr[i] );
+}
+console.log(Array(16).join("wat" - 1) + " Batman!" );
+
+ 
+
